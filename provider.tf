91894a8f27c0b00d69fb4607e7c49700092128e7
Rizqi Achmad Fauzy <rizqifauzy10@gmail.com>
2024-04-18 12:54:12 +0700

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,13 +1,17 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = "~>2.0"
+    }
+  }
+}
+
 provider "azurerm" {
   features {}
+
   #subscription_id   = "<azure_subscription_id>"
   #tenant_id         = "<azure_subscription_tenant_id>"
   #client_id         = "<service_principal_appid>"
   #client_secret     = "<service_principal_password>"
-
-  #OR on command Shell
-  #export ARM_SUBSCRIPTION_ID="<azure_subscription_id>"
-  #export ARM_TENANT_ID="<azure_subscription_tenant_id>"
-  #export ARM_CLIENT_ID="<service_principal_appid>"
-  #export ARM_CLIENT_SECRET="<service_principal_password>"
 }
