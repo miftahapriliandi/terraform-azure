137da6d2609a45d9d46842581666187572a2768f
Rizqi Achmad Fauzy <rizqifauzy10@gmail.com>
2024-04-18 13:43:01 +0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -17,6 +17,13 @@ resource "azurerm_subnet" "internal" {
   address_prefixes     = ["10.0.2.0/24"]
 }
 
+resource "azurerm_public_ip" "vm-public-ip" {
+  name                = "vm-public-ip"
+  location            = azurerm_resource_group.devops-rg.location
+  resource_group_name = azurerm_resource_group.devops-rg.name
+  allocation_method   = "Static"
+}
+
 resource "azurerm_network_interface" "main" {
   name                = "${var.prefix}-nic"
   location            = azurerm_resource_group.devops-rg.location
@@ -26,6 +33,7 @@ resource "azurerm_network_interface" "main" {
     name                          = "testconfiguration1"
     subnet_id                     = azurerm_subnet.internal.id
     private_ip_address_allocation = "Dynamic"
+    public_ip_address_id          = azurerm_public_ip.vm-public-ip.id
   }
 }
 
@@ -59,4 +67,31 @@ resource "azurerm_virtual_machine" "main" {
   tags = {
     environment = var.environment
   }
+}
+
+resource "azurerm_network_security_group" "azure-nsg" {
+  name                = "terraform-nsg"
+  location            = azurerm_resource_group.devops-rg.location
+  resource_group_name = azurerm_resource_group.devops-rg.name
+
+  security_rule {
+    name                       = "SSH"
+    priority                   = 1001
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "22"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+
+  tags = {
+    environment = "Production"
+  }
+}
+
+resource "azurerm_network_interface_security_group_association" "ngs-associate" {
+  network_interface_id      = azurerm_network_interface.main.id
+  network_security_group_id = azurerm_network_security_group.azure-nsg.id
 }
